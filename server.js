18ba20772e92bfae0a922466841622b161922f44
root <prosvirov.k@gmail.com>
2018-01-09 21:01:03 +0300

    Added random color to user

--- server.js
+++ server.js
@@ -28,8 +28,7 @@ io.sockets.on('connection', function(socket) {
 
     //send message
     socket.on('send message', function(data){
-        console.log(data);
-        io.sockets.emit('new message', {msg: data, user: socket.username});
+        io.sockets.emit('new message', {msg: data, user: socket.username, color: socket.color});
     });
 
     //new user
